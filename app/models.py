ae4e37f104984b6165961e2248de2427f579c177
Rogerio11 <55982331+Rogerio11@users.noreply.github.com>
2021-02-13 23:34:18 +0100

    changement model pour image lien

--- app/models.py
+++ app/models.py
@@ -13,9 +13,7 @@ class SiteWeb(models.Model):
     categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
     url = models.CharField(max_length = 1500)
     created_at = models.DateTimeField(auto_now_add=True)
-    def upload_path(self, filename):
-        return 'static/assets/images/%s_%s' % (self.created_at.strftime("%Y%m%d_%H%M%S"), filename)
-    image = models.FileField(upload_to=upload_path, blank = True, null=True)
+    image_lien = models.CharField(max_length = 25000)
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     visible = models.IntegerField(default= 1)
     
